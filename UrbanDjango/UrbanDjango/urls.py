ae03b3f275afb12b123cd4543ce87d8d35d1c45a
Abeille1990 <tatiana.tyukova@gmail.com>
2025-01-25 21:33:09 +0300

    Домашнее задание по теме "Urls и Views. Функциональное и классовое представление - Final

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from UrbanDjango.task2.views import hello
+# from UrbanDjango.task2.views import hello
 
 urlpatterns = [
-    path('', hello),
+    path('', TemplateView.as_view(template_name='second_task/greet.html')),
     path('class/', TemplateView.as_view(template_name='second_task/class_template.html')),
     path('function/', TemplateView.as_view(template_name='second_task/func_template.html')),
 ]
